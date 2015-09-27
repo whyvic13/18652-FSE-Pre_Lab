c41ca008116deda50193fbe2217ec8ea5cc6ae00
Haoyun Wen <haoyun_wen@hotmail.com>
2015-09-26 23:11:52 -0700

    delete node_modules
    change author

--- public/main.js
+++ public/main.js
@@ -1,6 +1,6 @@
 $(function() {
   var FADE_TIME = 150; // ms
-  var TYPING_TIMER_LENGTH = 400; // ms
+  var TYPING_TIMER_LENGTH = 5000; // ms
   var COLORS = [
     '#e21400', '#91580f', '#f8a700', '#f78b00',
     '#58dc00', '#287b00', '#a8f07a', '#4ae8c4',
